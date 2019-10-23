6c88c66899946f6e7ec46578d62e891194903ae2
cody0203 <congdinh2395@gmail.com>
2019-10-23 10:19:32 +0700

    Updates

---
+++ precache-manifest.335e97e4c99689b6d379809e381403e1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d2515d0e7a9c74a99959fc872296e9",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "5187a9a8bce493eac13a",
+    "url": "/react-quiz/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2c1cf915026382055b91",
+    "url": "/react-quiz/static/css/main.1b97c2e8.chunk.css"
+  },
+  {
+    "revision": "5187a9a8bce493eac13a",
+    "url": "/react-quiz/static/js/2.d4332ceb.chunk.js"
+  },
+  {
+    "revision": "2c1cf915026382055b91",
+    "url": "/react-quiz/static/js/main.75f67b83.chunk.js"
+  },
+  {
+    "revision": "23de2f01f24e601f99d6",
+    "url": "/react-quiz/static/js/runtime-main.9376759c.js"
+  },
+  {
+    "revision": "4b697e1e15f34ed9cad0cf83936a8fa5",
+    "url": "/react-quiz/static/media/background-sound.4b697e1e.mp3"
+  },
+  {
+    "revision": "8dd77828792cbba0e2fa6b706c1df939",
+    "url": "/react-quiz/static/media/correct.8dd77828.mp3"
+  },
+  {
+    "revision": "d1d9f1db11193ab57d65d74a67592996",
+    "url": "/react-quiz/static/media/quiz-background.d1d9f1db.svg"
+  },
+  {
+    "revision": "5526e83129e3fe6515d95b7160c85701",
+    "url": "/react-quiz/static/media/refreshing.5526e831.svg"
+  },
+  {
+    "revision": "0b4c861ac5951f68aa6844358a19728e",
+    "url": "/react-quiz/static/media/wrong.0b4c861a.mp3"
+  }
+]);
